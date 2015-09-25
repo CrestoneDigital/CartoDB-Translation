e8b89d8202e133e808fc58242bb155a4328f8f1c
tissatony <tisa.tekgile@gmail.com>
2015-09-25 17:35:16 +0530

    changes(25-sep)

--- translation-tool/translate.js
+++ translation-tool/translate.js
@@ -19,387 +19,32 @@ console.log(apiKey);
 var googleTranslate = require('google-translate')(apiKey);
 
 
-var languages = [
-   {
-    "language": "af",
-    "name": "Afrikaans"
-   },
-   {
-    "language": "sq",
-    "name": "Albanian"
-   },
-   {
-    "language": "ar",
-    "name": "Arabic"
-   },
-   {
-    "language": "hy",
-    "name": "Armenian"
-   },
-   {
-    "language": "az",
-    "name": "Azerbaijani"
-   },
-   {
-    "language": "eu",
-    "name": "Basque"
-   },
-   {
-    "language": "be",
-    "name": "Belarusian"
-   },
-   {
-    "language": "bn",
-    "name": "Bengali"
-   },
-   {
-    "language": "bs",
-    "name": "Bosnian"
-   },
-   {
-    "language": "bg",
-    "name": "Bulgarian"
-   },
-   {
-    "language": "ca",
-    "name": "Catalan"
-   },
-   {
-    "language": "ceb",
-    "name": "Cebuano"
-   },
-   {
-    "language": "ny",
-    "name": "Chichewa"
-   },
-   {
-    "language": "zh",
-    "name": "Chinese (Simplified)"
-   },
-   {
-    "language": "zh-TW",
-    "name": "Chinese (Traditional)"
-   },
-   {
-    "language": "hr",
-    "name": "Croatian"
-   },
-   {
-    "language": "cs",
-    "name": "Czech"
-   },
-   {
-    "language": "da",
-    "name": "Danish"
-   },
-   {
-    "language": "nl",
-    "name": "Dutch"
-   },
-   {
-    "language": "en",
-    "name": "English"
-   },
-   {
-    "language": "eo",
-    "name": "Esperanto"
-   },
-   {
-    "language": "et",
-    "name": "Estonian"
-   },
-   {
-    "language": "tl",
-    "name": "Filipino"
-   },
-   {
-    "language": "fi",
-    "name": "Finnish"
-   },
-   {
-    "language": "fr",
-    "name": "French"
-   },
-   {
-    "language": "gl",
-    "name": "Galician"
-   },
-   {
-    "language": "ka",
-    "name": "Georgian"
-   },
-   {
-    "language": "de",
-    "name": "German"
-   },
-   {
-    "language": "el",
-    "name": "Greek"
-   },
-   {
-    "language": "gu",
-    "name": "Gujarati"
-   },
-   {
-    "language": "ht",
-    "name": "Haitian Creole"
-   },
-   {
-    "language": "ha",
-    "name": "Hausa"
-   },
-   {
-    "language": "iw",
-    "name": "Hebrew"
-   },
-   {
-    "language": "hi",
-    "name": "Hindi"
-   },
-   {
-    "language": "hmn",
-    "name": "Hmong"
-   },
-   {
-    "language": "hu",
-    "name": "Hungarian"
-   },
-   {
-    "language": "is",
-    "name": "Icelandic"
-   },
-   {
-    "language": "ig",
-    "name": "Igbo"
-   },
-   {
-    "language": "id",
-    "name": "Indonesian"
-   },
-   {
-    "language": "ga",
-    "name": "Irish"
-   },
-   {
-    "language": "it",
-    "name": "Italian"
-   },
-   {
-    "language": "ja",
-    "name": "Japanese"
-   },
-   {
-    "language": "jw",
-    "name": "Javanese"
-   },
-   {
-    "language": "kn",
-    "name": "Kannada"
-   },
-   {
-    "language": "kk",
-    "name": "Kazakh"
-   },
-   {
-    "language": "km",
-    "name": "Khmer"
-   },
-   {
-    "language": "ko",
-    "name": "Korean"
-   },
-   {
-    "language": "lo",
-    "name": "Lao"
-   },
-   {
-    "language": "la",
-    "name": "Latin"
-   },
-   {
-    "language": "lv",
-    "name": "Latvian"
-   },
-   {
-    "language": "lt",
-    "name": "Lithuanian"
-   },
-   {
-    "language": "mk",
-    "name": "Macedonian"
-   },
-   {
-    "language": "mg",
-    "name": "Malagasy"
-   },
-   {
-    "language": "ms",
-    "name": "Malay"
-   },
-   {
-    "language": "ml",
-    "name": "Malayalam"
-   },
-   {
-    "language": "mt",
-    "name": "Maltese"
-   },
-   {
-    "language": "mi",
-    "name": "Maori"
-   },
-   {
-    "language": "mr",
-    "name": "Marathi"
-   },
-   {
-    "language": "mn",
-    "name": "Mongolian"
-   },
-   {
-    "language": "my",
-    "name": "Myanmar (Burmese)"
-   },
-   {
-    "language": "ne",
-    "name": "Nepali"
-   },
-   {
-    "language": "no",
-    "name": "Norwegian"
-   },
-   {
-    "language": "fa",
-    "name": "Persian"
-   },
-   {
-    "language": "pl",
-    "name": "Polish"
-   },
-   {
-    "language": "pt",
-    "name": "Portuguese"
-   },
-   {
-    "language": "pa",
-    "name": "Punjabi"
-   },
-   {
-    "language": "ro",
-    "name": "Romanian"
-   },
-   {
-    "language": "ru",
-    "name": "Russian"
-   },
-   {
-    "language": "sr",
-    "name": "Serbian"
-   },
-   {
-    "language": "st",
-    "name": "Sesotho"
-   },
-   {
-    "language": "si",
-    "name": "Sinhala"
-   },
-   {
-    "language": "sk",
-    "name": "Slovak"
-   },
-   {
-    "language": "sl",
-    "name": "Slovenian"
-   },
-   {
-    "language": "so",
-    "name": "Somali"
-   },
-   {
-    "language": "es",
-    "name": "Spanish"
-   },
-   {
-    "language": "su",
-    "name": "Sundanese"
-   },
-   {
-    "language": "sw",
-    "name": "Swahili"
-   },
-   {
-    "language": "sv",
-    "name": "Swedish"
-   },
-   {
-    "language": "tg",
-    "name": "Tajik"
-   },
-   {
-    "language": "ta",
-    "name": "Tamil"
-   },
-   {
-    "language": "te",
-    "name": "Telugu"
-   },
-   {
-    "language": "th",
-    "name": "Thai"
-   },
-   {
-    "language": "tr",
-    "name": "Turkish"
-   },
-   {
-    "language": "uk",
-    "name": "Ukrainian"
-   },
-   {
-    "language": "ur",
-    "name": "Urdu"
-   },
-   {
-    "language": "uz",
-    "name": "Uzbek"
-   },
-   {
-    "language": "vi",
-    "name": "Vietnamese"
-   },
-   {
-    "language": "cy",
-    "name": "Welsh"
-   },
-   {
-    "language": "yi",
-    "name": "Yiddish"
-   },
-   {
-    "language": "yo",
-    "name": "Yoruba"
-   },
-   {
-    "language": "zu",
-    "name": "Zulu"
-   }
-  ];
+var languages = [{
+		"language": "af",
+		"name": "Afrikaans"
+	},{
+		"language": "sq",
+		"name": "Albanian"
+	},{
+		"language": "ar",
+		"name": "Arabic"
+	}];
 
 var fnAr = [], data = {};
-var masterData = JSON.parse(fs.readFileSync(path.resolve(path.join('../languages','master.js')),'utf8').replace('var cdb_lang_file = ', ''));
+var masterData = JSON.parse(fs.readFileSync(path.resolve(path.join('../languages','master1.js')),'utf8').replace('var cdb_lang_file = ', ''));
+
+// console.log(masterData);
+
 
 // Create missing files
 
 _.each(languages, function(lng){
 	var filename = lng.language + '.i18n.js';
 	var lnprefix = lng.language;
-			console.log(filename);
 	fnAr.push(function(cb) {
-			console.log(filename, ' -- ');
 		fs.access(path.resolve(path.join('../languages',filename)), function (err) {
 			if(err) {
-				fs.createReadStream(path.resolve(path.join('../languages/old','jp.i18n.js'))).pipe(fs.createWriteStream(path.resolve(path.join('../languages',filename))));
+				fs.createReadStream(path.resolve(path.join('../languages/old','tpl.i18n.js'))).pipe(fs.createWriteStream(path.resolve(path.join('../languages',filename))));
 			}
 			cb();
 		});		
@@ -444,7 +89,7 @@ function translateUpdateProcess(key, callback) {
 				dtReplace = dt.substring(dt.indexOf('//start') + '//start'.length, dt.indexOf('//end'));
 				var dtObj = JSON.parse(dtReplace.trim());
 				dtObj[key] = translation.translatedText;
-				dt = dt.replace(dtReplace,JSON.stringify(dtObj));
+				dt = dt.replace(dtReplace,"\r\n" + JSON.stringify(dtObj) + "\r\n");
 				fs.writeFile(path.resolve(path.join('../languages',lng.language + '.i18n.js')), dt, 'utf8', function(err){
 					cb();
 				});
@@ -456,34 +101,3 @@ function translateUpdateProcess(key, callback) {
 	});
 }
 
-
-/*	
-	
-	
-	//console.log(languages[i].language);
-	googleTranslate.translate('Adding new column', 'en', languages[i].language, function (err, translation) {
-		console.log(translation.translatedText);
-		fs.writeFileSync('../languages/old/'+languages[i].language+'.i18n.js', fs.readFileSync('../languages/master.js'));
-		var content;
-        console.log(content);
-        fs.readFile(path.join(__dirname,"../languages/old/",languages[i].language+'.i18n.js'), 'utf8',function (err,data) {
-            if (err) {
-                console.log(err);
-                process.exit(1);
-            }
-            content = util.format(data); 
-			
-var temp = new Array();
-// this will return an array with strings "1", "2", etc. 
-content = content.replace(/(\r\n|\n|\r)/gm,"");
-content = content.replace("\": false,","");
-content = content.replace("\',","");
-content = content.replace("',","");
-temp = content.split("\": false");
-
-
-            console.log(temp);
-        });
-	});
-}
-*/
